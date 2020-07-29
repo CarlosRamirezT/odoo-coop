3a06742ab923a4b8865ad00f93f122751da9360c
Carlos Ramirez <carlosdanielrt@gmail.com>
2020-07-29 15:13:45 -0400

    [ADD] coop_management: scaffold

---
+++ coop_management/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Cooperative Management",
+    "summary": """
+        Cooperative Management
+    """,
+    "description": """
+        Cooperative Management
+    """,
+    "author": "Cadara",
+    "website": "http://cadara.epizy.com/",
+    "category": "Accounting &amp; Finance",
+    "version": "12.0.1.0.0",
+    "depends": ["base"],
+    "data": [],
+    "demo": [],
+    'application': True,
+}
