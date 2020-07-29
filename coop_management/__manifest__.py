578dc24a9544cedc5f9f6989cd8cd867114583ab
Carlos Ramirez <carlosdanielrt@gmail.com>
2020-07-29 16:31:15 -0400

    [ADD] coop_security: security groups

--- coop_management/__manifest__.py
+++ coop_management/__manifest__.py
@@ -10,10 +10,8 @@
     "website": "http://cadara.epizy.com/",
     "category": "Accounting &amp; Finance",
     "version": "12.0.1.0.0",
-    "depends": [
-        "account",
-    ],
-    "data": [],
+    "depends": ["account",],
+    "data": ["security/coop_secutiry.xml"],
     "demo": [],
-    'application': True,
+    "application": True,
 }
