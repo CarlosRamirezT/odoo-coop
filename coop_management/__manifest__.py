f2ac7e2ce11a63529a44f59885ae0dfc47d1a744
Carlos Ramirez <carlosdanielrt@gmail.com>
2020-07-29 15:17:03 -0400

    [ADD] coop_management: dependencies

--- coop_management/__manifest__.py
+++ coop_management/__manifest__.py
@@ -10,7 +10,9 @@
     "website": "http://cadara.epizy.com/",
     "category": "Accounting &amp; Finance",
     "version": "12.0.1.0.0",
-    "depends": ["base"],
+    "depends": [
+        "account",
+    ],
     "data": [],
     "demo": [],
     'application': True,
