6ab537ce264a1cec9573b3bc7913428001ee3c35
Carlos Ramirez <carlosdanielrt@gmail.com>
2020-08-04 03:01:44 -0400

    [ADD] coop_management: account_views

--- coop_management/models/account.py
+++ coop_management/models/account.py
@@ -4,11 +4,13 @@ from odoo import api, fields, models
 class AccountAccount(models.Model):
     _inherit = "account.account"
 
+    name = fields.Char(required=False)
+    user_type_id = fields.Many2one(required=False)
     type = fields.Selection([("savings", "Savings")])
     balance = fields.Float("Current Balance", compute="_compute_balance", store=True)
     open_balance = fields.Float("Opening Balance")
     move_line_ids = fields.One2many(
-        "account.move.line",
+        "account.account.move",
         "account_id",
         string="Account Moves",
     )
@@ -17,3 +19,11 @@ class AccountAccount(models.Model):
     def _compute_balance(self):
         # TODO: Implement balance computation
         self.balance = self.balance
+
+    @api.multi
+    def name_get(self):
+        result = []
+        for account in self:
+            name = account.code
+            result.append((account.id, name))
+        return result
--- coop_management/models/account_move.py
+++ coop_management/models/account_move.py
@@ -2,7 +2,8 @@ from odoo import fields, models
 
 
 class AccountMoveLine(models.Model):
-    _inherit = "account.move.line"
+    _name = "account.account.move"
+    _order = "create_date"
 
     type = fields.Selection(
         [
@@ -11,4 +12,8 @@ class AccountMoveLine(models.Model):
             ("other_bank", "Other Banks"),
         ]
     )
+    account_id = fields.Many2one("account.account", "Account")
     account_dest_id = fields.Many2one("account.account", "Destination Account")
+    debit_amount = fields.Float("Debit")
+    credit_amount = fields.Float("Credit")
+    balance = fields.Float("Balance")
